538059de79c59ff3ce031e9d7d7fe868d625aa74
kiranpanchavati9 <kiranpanchavati9@gmail.com>
2026-08-02 14:43:30 +0400

    outputs updated

--- modules/Auto-Scaling-Group/outputs.tf
+++ modules/Auto-Scaling-Group/outputs.tf
@@ -1,9 +1,14 @@
 output "autoscaling_group_id" {
   description = "The ID of the autoscaling group"
-  value       = aws_autoscaling_group.search-head-autoscaling-group.id
+  value       = aws_autoscaling_group.search_head.id
 }
 
 output "autoscaling_group_name" {
   description = "The name of the autoscaling group"
-  value       = aws_autoscaling_group.search-head-autoscaling-group.name
+  value       = aws_autoscaling_group.search_head.name
+}
+
+output "autoscaling_group_arn" {
+  description = "The ARN of the autoscaling group"
+  value       = aws_autoscaling_group.search_head.arn
 }
--- modules/Launch-Template/outputs.tf
+++ modules/Launch-Template/outputs.tf
@@ -1,9 +1,19 @@
-output "launch_template_id" {
+output "id" {
   description = "The ID of the launch template"
   value       = aws_launch_template.splunk_search_head.id
 }
 
-output "launch_template_name" {
+output "latest_version" {
+  description = "The latest version of the launch template"
+  value       = aws_launch_template.splunk_search_head.latest_version
+}
+
+output "name" {
   description = "The name of the launch template"
   value       = aws_launch_template.splunk_search_head.name
+}
+
+output "arn" {
+  description = "The ARN of the launch template"
+  value       = aws_launch_template.splunk_search_head.arn
 }
