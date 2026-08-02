41ebed09a3b2c85be189d9f5950d4a62b8dbe004
kiranpanchavati9 <kiranpanchavati9@gmail.com>
2026-08-02 14:08:21 +0400

    updated scripts auto scaling

--- main.tf
+++ main.tf
@@ -1,10 +1,22 @@
 module "splunk_search_head" {
     source = "./modules/Launch-Template"
-    image_id = "ami-0220d79f3f480ecf5"
-    instance_type = "t3.small"
-    key_name = "aws-helpag"
-    instance_initiated_shutdown_behavior = "terminate"
+    image_id = var.image_id
+    instance_type = var.instance_type
+    key_name = var.key_name
+    instance_initiated_shutdown_behavior = var.instance_initiated_shutdown_behavior
     monitoring = true
     vpc_security_group_ids = ["sg-02d2ebc3b9af4f158"]
     user_data = filebase64("${path.module}/splunk-search-head.sh")
+}
+
+module "splunk_search_head_autoscaling_group" {
+    source = "./modules/Auto-Scaling-Group"
+    vpc_zone_identifier = var.vpc_zone_identifier
+    health_check_type = var.health_check_type
+    max_size = var.max_size
+    min_size = var.min_size
+    health_check_grace_period =  var.health_check_grace_period
+    desired_capacity = var.desired_capacity
+    heartbeat_timeout = var.heartbeat_timeout
+    launch_template_name = var.launch_template_name
 }
---
+++ variables.tf
@@ -0,0 +1,102 @@
+variable "name" {
+  description = "Name prefix for the launch template and instances"
+  type        = string
+}
+
+variable "image_id" {
+  description = "AMI ID for the search head"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "EC2 key pair name"
+  type        = string
+}
+
+variable "instance_initiated_shutdown_behavior" {
+  description = "Behaviour on in-guest shutdown: stop or terminate"
+  type        = string
+}
+
+variable "monitoring" {
+  description = "Enable detailed CloudWatch monitoring"
+  type        = bool
+}
+
+variable "vpc_security_group_ids" {
+  description = "Security group IDs to attach"
+  type        = list(string)
+}
+
+variable "user_data" {
+  description = "Base64-encoded user data script"
+  type        = string
+}
+
+variable "iam_instance_profile" {
+  description = "IAM instance profile name"
+  type        = string
+}
+
+variable "root_device_name" {
+  description = "Root device name, /dev/xvda on Amazon Linux, /dev/sda1 on RHEL"
+  type        = string
+}
+
+variable "root_volume_size" {
+  description = "Root volume size in GB"
+  type        = number
+}
+
+variable "tags" {
+  description = "Tags applied to instances and volumes"
+  type        = map(string)
+}
+
+## Auto-Scaling Group Variables
+
+variable "vpc_zone_identifier" {
+  description = "The VPC zone identifier"
+  type        = list(string)
+}
+
+variable "health_check_type" {
+  description = "The health check type"
+  type        = string
+}
+
+variable "max_size" {
+  description = "The maximum size of the autoscaling group"
+  type        = number
+}
+
+variable "min_size" {
+  description = "The minimum size of the autoscaling group"
+  type        = number
+
+}
+
+variable "health_check_grace_period" {
+  description = "The health check grace period"
+  type        = number
+}
+
+variable "desired_capacity" {
+  description = "The desired capacity of the autoscaling group"
+  type        = number
+}
+
+variable "heartbeat_timeout" {
+  description = "The heartbeat timeout of the autoscaling group"
+  type        = number
+}
+
+variable "launch_template_name" {
+  description = "The name of the launch template"
+  type        = string
+}
