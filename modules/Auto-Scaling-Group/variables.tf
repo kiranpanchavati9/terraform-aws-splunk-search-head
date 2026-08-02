ca7e40a3bba9f46660b3142e2a80e914f233794d
kiranpanchavati9 <kiranpanchavati9@gmail.com>
2026-08-02 14:35:47 +0400

    Restore ASG module variables

--- modules/Auto-Scaling-Group/variables.tf
+++ modules/Auto-Scaling-Group/variables.tf
@@ -14,3 +14,38 @@ variable "target_group_arns" {
   type        = list(string)
   default     = []
 }
+
+variable "vpc_zone_identifier" {
+  description = "Subnet IDs, one per AZ"
+  type        = list(string)
+}
+
+variable "health_check_type" {
+  description = "EC2 or ELB. Use EC2 until a target group is attached."
+  type        = string
+}
+
+variable "max_size" {
+  description = "Maximum ASG size"
+  type        = number
+}
+
+variable "min_size" {
+  description = "Minimum ASG size"
+  type        = number
+}
+
+variable "health_check_grace_period" {
+  description = "Seconds before health checks begin"
+  type        = number
+}
+
+variable "desired_capacity" {
+  description = "Number of search head cluster members, must be odd"
+  type        = number
+}
+
+variable "heartbeat_timeout" {
+  description = "Seconds the termination hook waits for splunk offline"
+  type        = number
+}
